345873d2564f4be445446738969db71661d59e35
nikitadpopel <nikita.d.popel@gmail.com>
2023-05-12 08:44:41 -0400

    got started with helper math

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include "math_helpers.h"
 
 int main(int argc,char* argv[])
 {
-	std::cout << "test" <<std::endl;
+	std::vector<double> mytemp;
+	mytemp.push_back(5);
+	mytemp.push_back(6);
+	std::vector<double> newtemp = standardize(std::move(mytemp));
+	
+	std::vector<double>::iterator iter;
+	for(iter = newtemp.begin(); iter < newtemp.end(); iter++)
+	{
+		std::cout << *iter << std::endl;
+	}
+	
 	return 0;
 }
---
+++ src/math_helpers.cpp
@@ -0,0 +1,7 @@
+#include "math_helpers.h"
+
+std::vector<double> standardize(std::vector<double> data)
+{
+    std::vector<double> temp = data;
+    return std::move(temp);
+}
